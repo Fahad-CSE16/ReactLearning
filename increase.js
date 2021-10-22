1f2f44aadc7b2e8f0094eba45c32b028901a7773
Fahad-CSE16 <mdfahadhossain71@gmail.com>
2021-10-23 02:49:35 +0600

    decrement function practised

--- increase.js
+++ increase.js
@@ -4,11 +4,16 @@ const Child1=()=>{
     const [value, myFunction] = React.useState(0);
     return (
         <div className="text-center p-4">
+            <button className="btn btn-outline-primary"
+            onClick={()=>myFunction(value-1)}
+            >
+                Decrement -</button>
             <h1 >{value}</h1>
             <button className="btn btn-outline-primary"
             onClick={()=>myFunction(value+1)}
             >
                 Increment +</button>
+                <hr />
         </div>
     )
 }
